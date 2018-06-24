d88e2b1d0027ac6d2a436cfe3774e332de6addb2
jerry <jamooyman@gmail.com>
2018-06-24 16:40:47 +1000

    added sound demo project

---
+++ SoundDemo/app/src/main/java/com/mooyz/sounddemo/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.mooyz.sounddemo
+
+import android.app.Activity
+import android.content.Context
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.annotation.IdRes
+import android.util.Log
+import android.view.View
+import android.widget.SeekBar
+
+class MainActivity : AppCompatActivity() { //, SeekBar.OnSeekBarChangeListener {
+
+    var mplayer:  MediaPlayer? = null
+    var audioManager : AudioManager? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mplayer = MediaPlayer.create(this, R.raw.birds)
+        audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        val maxVol = audioManager!!.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+        val curVol = audioManager!!.getStreamVolume(AudioManager.STREAM_MUSIC)
+
+        val volumeController: SeekBar = bind(R.id.volumeController)
+        volumeController.max = maxVol
+        volumeController.progress = curVol
+
+        volumeController.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                Log.i("seekbar value: ", progress.toString())
+
+                audioManager!!.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0)
+            }
+        })
+    }
+
+    fun play(view: View) {
+        mplayer!!.start()
+    }
+
+    fun pause(view: View) {
+        mplayer!!.pause()
+    }
+}
+
+
+fun <T : View> Activity.bind(@IdRes res : Int) : T {
+    @Suppress("UNCHECKED_CAST")
+    return findViewById(res) as T
+}
